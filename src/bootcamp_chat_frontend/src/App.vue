dbc257916d1574f68719556b248f4627a570d2f7
Adolmeister <adrianko4646@gmail.com>
2024-07-30 16:02:41 +0200

    auth and button

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,28 +1,42 @@
 <script lang="ts">
 import { ref } from 'vue';
 import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
-let greeting = ref('');
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
 
-export default{
+export default {
   data() {
     return {
-      newNote:"",
-      notes : [] as string[]
+      newNote: "",
+      notes: [] as string[],
+      identity: undefined as undefined | Identity,
     }
   },
   methods: {
-    async dodajNotatke(){
+    async dodajNotatke() {
       await bootcamp_chat_backend.add_note(this.newNote)
       await this.pobierzNotatki()
     },
-    async pobierzNotatki(){
+    async pobierzNotatki() {
       this.notes = await bootcamp_chat_backend.get_notes()
+    },
+    async login() {
+      const authClient = await AuthClient.create();
+      await authClient.login({
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
+      })
+
+      const identity = authClient.getIdentity();
+      console.log("Zalogowano", identity.getPrincipal())
+      this.identity = identity;
+      
+     //const agent = new HttpAgent({ identity });
     }
   },
-  mounted() {
+  mounted(){
     this.pobierzNotatki()
-  },
-  
+  }
 }
 </script>
 
@@ -31,14 +45,12 @@ export default{
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-
+    {{ identity?.getPrincipal() }} <button @click="login">login</button>
     <div>
       {{ notes }}
     </div>
-
     <div>
       <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
     </div>
-  
   </main>
 </template>
